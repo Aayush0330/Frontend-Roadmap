c6eb874944fdbd1c7b97673ae5f59bf7211dcc83
Aayush <aayushchandak2@gmail.com>
2025-07-09 21:09:32 +0530

    add

---
+++ Javascript Basics/Variables+Project/variables.js
@@ -0,0 +1,36 @@
+/* 
+Variable ya box 
+
+What is a Variable?
+Jab hum kisi cheez ko yaad rakhna chahte hain (jaise naam, age, game name), to hum usse ek box (variable) mein rakhte hain.
+
+Variable 3 types ke hote hai :-
+
+1.var Purana style (avoid karte hain ab)
+
+2.let Normal use (mostly used)
+
+3.const Fix value (change nahi kar sakte)
+
+*/
+
+let myName = "Aayush"
+let myAge = 21
+let myFavGame = "RE3"
+
+console.log("Name:" , myName )
+console.log("Age:" , myAge )
+console.log("Favourate Game:" , myFavGame)
+
+/*
+
+💻 Project 1: My Info Card App
+
+🔧 Goal:
+
+3 variables banane hain: name, age, game
+Ek simple page banana hai jo inhe display kare
+
+*/
+
+// Template literals ${}
--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
 # Days   |   Dates   |   Topics   |   Projects
-# Day 1   |   9 Jul 2025   |   Variables   |   My Info Card App
+# Day 1   |   9 Jul 2025   |   Variables , Datatypes  |   My Info Card App , Type Checker App
